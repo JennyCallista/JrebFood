2ccf042235bb438588911ff69fe45a3cc10feb06
GilbertNicholas <gilbertnicholas09@gmail.com>
2020-12-07 21:44:16 +0700

    BASE PROJECT READY

---
+++ src/model/UserModel.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.Vector;
+
+import core.model.Model;
+
+public class UserModel extends Model{
+
+	public UserModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vector<Model> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/view/LoginView.java
@@ -0,0 +1,29 @@
+package view;
+
+import core.view.View;
+
+public class LoginView extends View{
+
+	public LoginView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void addComponent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void addListener() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
